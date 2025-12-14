6f77931445214f0ba9d1aca3a7dc81f9e3378574
hwanzanghagetne <dltmdghks543@gmail.com>
2025-12-15 01:17:06 +0900

    fix: 아이템 조회 기능 추가

--- src/main/java/com/jaegokeeper/hwan/service/ItemService.java
+++ src/main/java/com/jaegokeeper/hwan/service/ItemService.java
@@ -1,8 +1,13 @@
 package com.jaegokeeper.hwan.service;
 
+import com.jaegokeeper.hwan.dto.ItemListDTO;
 import com.jaegokeeper.hwan.dto.ItemCreateRequestDTO;
+
+import java.util.List;
 
 public interface ItemService {
 
     Integer registerItem(ItemCreateRequestDTO itemCreateRequestDTO);
+
+    List<ItemListDTO> getItemList(Integer storeId);
 }
